16212f71d85f403f6844cdf51b5394c1ccce5eae
jaeminjeon <rmk1075@gmail.com>
2024-01-23 16:14:59 +0900

    init: initialize springboot project

---
+++ spring-java/src/test/java/com/exercise/springjava/SpringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.springjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
