6eae86d7ad43be105f8270c6173aa81241cc0e89
jaeminjeon <rmk1075@gmail.com>
2024-01-23 18:38:14 +0900

    feat: init LambdaExercise

---
+++ spring-java/src/test/java/com/exercise/springjava/lambda/LambdaExerciseTest.java
@@ -0,0 +1,60 @@
+package com.exercise.springjava.lambda;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class LambdaExerciseTest {
+
+  private final LambdaExercise lambdaExercise = new LambdaExercise();
+  private static List<Long> numbers = new LinkedList<>();
+  private static Long limit = 100L;
+  private Long startTime;
+
+  @BeforeAll
+  static void setUpAll() {
+    Random random = new Random();
+    int size = 5000000;
+    for (int i = 0; i < size; i++) {
+      numbers.add(random.nextLong(10000L));
+    }
+  }
+
+  @BeforeEach
+  void setUp() {
+    startTime = System.currentTimeMillis();
+  }
+
+  @AfterEach
+  void tearDown() {
+    System.out.println("Time elapsed: " + (System.currentTimeMillis() - startTime) + "ms");
+  }
+
+  @Test
+  void testFilterValue() {
+    lambdaExercise.filterValue(numbers, limit);
+  }
+
+  @Test
+  void testFilterValueWithFilterInstance() {
+    lambdaExercise.filterValueWithFilterInstance(
+      numbers, lambdaExercise.new NumberFilter(limit));
+  }
+
+  @Test
+  void testFilterValueWithPredicate() {
+    lambdaExercise.filterValueWithPredicate(
+      numbers, number -> number > limit);
+  }
+
+  @Test
+  void testFilterValueWithStream() {
+    lambdaExercise.filterValueWithStream(
+      numbers, number -> number > limit);
+  }
+}
