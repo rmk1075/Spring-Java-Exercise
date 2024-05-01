f861a297f80735248d5c8a949e33f22feb0439e7
jamie <rmk1075@gmail.com>
2024-05-01 15:20:35 +0900

    feat: add empty task comparison

--- spring-java/src/main/java/com/exercise/springjava/virtualThread/controller/VirtualThreadExerciseController.java
+++ spring-java/src/main/java/com/exercise/springjava/virtualThread/controller/VirtualThreadExerciseController.java
@@ -2,13 +2,15 @@ package com.exercise.springjava.virtualThread.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.exercise.springjava.virtualThread.controller.request.ExecutionEmptyRequest;
+import com.exercise.springjava.virtualThread.controller.response.ExecutionTimeResponse;
 import com.exercise.springjava.virtualThread.controller.response.ThreadId;
 import com.exercise.springjava.virtualThread.service.VirtualThreadExerciseService;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -28,12 +30,24 @@ public class VirtualThreadExerciseController {
     }
 
     // Execute empty task by virtual-thread
+    @PutMapping("/virtual/empty")
+    public ExecutionTimeResponse executeEmptyTaskByVirtualThread(@RequestBody ExecutionEmptyRequest request) {
+      long start = System.currentTimeMillis();
+      threadService.executeEmptyTaskByVirtualThread(request.getNum());
+      return new ExecutionTimeResponse(System.currentTimeMillis() - start);
+    }
 
     // Execute CPU bound task by virtual-thread
 
     // Execute IO bound task by virtual-thread
 
     // Execute empty task by thread
+    @PutMapping("/thread/empty")
+    public ExecutionTimeResponse executeEmptyTaskByThread(@RequestBody ExecutionEmptyRequest request) {
+      long start = System.currentTimeMillis();
+      threadService.executeEmtpyTaskByThread(request.getNum());
+      return new ExecutionTimeResponse(System.currentTimeMillis() - start);
+    }
 
     // Execute CPU bound task by thread
 
--- spring-java/src/main/java/com/exercise/springjava/virtualThread/service/VirtualThreadExerciseService.java
+++ spring-java/src/main/java/com/exercise/springjava/virtualThread/service/VirtualThreadExerciseService.java
@@ -1,5 +1,8 @@
 package com.exercise.springjava.virtualThread.service;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,5 +14,19 @@ public class VirtualThreadExerciseService {
           System.out.println("virtual thread: " + Thread.currentThread().threadId());
         }
     );
+  }
+
+  public void executeEmptyTaskByVirtualThread(int n) {
+    List<Thread> threads = IntStream.range(0, n)
+      .mapToObj(i -> Thread.ofVirtual().unstarted(() -> {}))
+      .toList();
+    threads.forEach(Thread::start);
+  }
+
+  public void executeEmtpyTaskByThread(int n) {
+    List<Thread> threads = IntStream.range(0, n)
+      .mapToObj(i -> new Thread(() -> {}))
+      .toList();
+    threads.forEach(Thread::start);
   }
 }
