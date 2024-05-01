fb46dd53c4688faa28e5fc2b5d2ded10266611da
jamie <rmk1075@gmail.com>
2024-05-01 15:20:35 +0900

    feat: add empty task comparison

---
+++ spring-java/src/main/java/com/exercise/springjava/virtualThread/controller/request/ExecutionEmptyRequest.java
@@ -0,0 +1,8 @@
+package com.exercise.springjava.virtualThread.controller.request;
+
+import lombok.Data;
+
+@Data
+public class ExecutionEmptyRequest {
+  private int num = 0;
+}
---
+++ spring-java/src/main/java/com/exercise/springjava/virtualThread/controller/response/ExecutionTimeResponse.java
@@ -0,0 +1,8 @@
+package com.exercise.springjava.virtualThread.controller.response;
+
+import lombok.Data;
+
+@Data
+public class ExecutionTimeResponse {
+  private final long executionTime;
+}
